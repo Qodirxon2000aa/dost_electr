1ab7c5051bdf10627a8103fadfd5d02f9d293d05
Qodirxon2000aa <namesuz41@gmail.com>
2026-02-21 07:15:14 +0500

    Sas

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_URL || 'https://dost-api.vercel.app/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://dost-api2.vercel.app/api';
 
 const request = async (method, path, body = null) => {
   const options = {
